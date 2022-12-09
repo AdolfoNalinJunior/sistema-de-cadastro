f0c6e41fc72611887f3cbc42b37a6daffaea7801
Adolfo Nalin Junior <adolfonalin22@gmail.com>
2022-12-09 20:48:39 -0300

    Estou voltando para o código origem

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Aplicação está se conectando ao banco de dados");
-// Mais um console log de teste:
-console.log("O Paulo fez esse commit sem você ver na aula!")
-console.log("Para mim saber onde eu vou, como que eu faço?")
-console.log("Eu sei que nada sei");
